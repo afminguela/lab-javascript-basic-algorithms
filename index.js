f9c91e7bfb98e5c18b106e0c0b171f3c5a686907
afminguela <afminguela@gmail.com>
2025-06-17 12:24:06 +0200

    ejercicios 1 y 2 hechos

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 // Iteration 1: Names and Input
+// 1.1 Create a variable hacker1 with the driver's name.
+let hacker1= "jose Luis"
+// 1.2 Print "The driver's name is XXXX".
+console.log("The Driver´s name is "+ hacker1)
+// 1.3 Create a variable hacker2 with the navigator's name.
+let hacker2 = "garmin"
+// 1.4 Print "The navigator's name is YYYY".
+
+console.log("The navigator's name is " + hacker2)
 
 
 // Iteration 2: Conditionals
+
+// 2.1. Depending on which name [is longer](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length), print:
+//   <br>
+//     - `The driver has the longest name, it has XX characters.` or <br>
+//     - `It seems that the navigator has the longest name, it has XX characters.` or <br>
+//     - `Wow, you both have equally long names, XX characters!`.
+
+let fulano= hacker1.length;
+let mengano= hacker2.length;
+
+if(fulano > mengano){
+    console.log(`The driver has the longest name, it has ${fulano} characters.`)
+
+} else if(fulano > mengano) {
+       console.log(`It seems that the navigator has the longest name, it has ${mengano} characters.`)
+} else {
+    console.log(`Wow, you both have equally long names, XX characters!`)
+}
 
 
 // Iteration 3: Loops
